2a1641c0b40c0fdd85ae5de6a15ac447bbf070d7
toshie <toshie@debian@tushar.com>
2024-12-20 12:39:16 +0530

    new pid code

--- pid/pid.ino
+++ pid/pid.ino
@@ -1,29 +1,27 @@
 int ena = 3;
+int enb = 5;
 int in1 = 6;
 int in2 = 7;
-int enb = 5;
 int in3 = 9;
 int in4 = 8;
 
-float P;
-float I;
-float D;
+int P, D, I, previousError, PIDvalue, error;
+int lsp, rsp;
 
-float Kp = 0.3;
-float Ki = 0.1;
-float Kd = 0.05;
+float Kp = 0.05;
+float Ki = 0.0;
+float Kd = 0.0;
 
-float previousError = 0;
 
 int lfspeed = 60;
-int rfspeed = 70;
+int rfspeed = 60;
 
-int threshold = 700;
+int threshold = 400;
 
 
 void linefollow()
-{ //green () ;
-  while ((analogRead(0) < threshold || analogRead(2) < threshold || analogRead(1) < threshold || analogRead(3) < threshold) || analogRead(4) < threshold)
+{
+  while (analogRead(2) > threshold || analogRead(1) > threshold || analogRead(3) > threshold)
   {
     PID();
   }
@@ -32,59 +30,50 @@ void linefollow()
 }
 void PID()
 {
-  int lsp, rsp;
   int error = (analogRead(0)*1.5 + analogRead(1)) - (analogRead(3) + analogRead(4)*1.5);
   P = error;
   I = I + error;
   D = error - previousError;
 
-  I = constrain(I, -100, 100);
+  I = constrain(I, -1000, 1000);
 
   int PIDvalue = (Kp * P) + (Ki * I) + (Kd * D);
   previousError = error;
 
-
   lsp = lfspeed - PIDvalue;
   rsp = rfspeed + PIDvalue;
 
-
-  if (lsp > 120) {
-    lsp = 120;
+  if (lsp > 90) {
+    lsp = 90;
   }
 if (lsp < 0) {
     lsp = 0;
   }
-  if (rsp > 120) {
-    rsp = 120;
+  if (rsp > 90) {
+    rsp = 90;
   }
   if (rsp < 0) {
     rsp = 0;
   }
-  // Serial.println(rsp);
 
   digitalWrite(in1, HIGH);
   digitalWrite(in3, HIGH);
-  // Serial.println("Lsp : ");
-  // Serial.println(lsp);
   analogWrite(ena, lsp);
   analogWrite(enb,rsp);
 }
 
 
 void setup() {
-    Serial.begin(9600); // Start serial communication
+    Serial.begin(9600);
 
-  // put your setup code here, to run once:
   pinMode(ena, OUTPUT);
   pinMode(in1, OUTPUT);
   pinMode(in2, OUTPUT);
   pinMode(enb, OUTPUT);
   pinMode(in3, OUTPUT);
   pinMode(in4, OUTPUT);
-
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   linefollow();
 }
